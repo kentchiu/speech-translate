cae9fadd6e03717579ec0faaa2da3445f2713e66
Kent Chiu <kent.cwg@gmail.com>
2024-09-14 22:11:45 +0800

    add expect

--- asr1.py
+++ asr1.py
@@ -1,21 +1,9 @@
 import os
 import time
-import warnings
 
 import whisper
 import torch
 import langcodes
-
-warnings.filterwarnings("ignore", category=UserWarning, module="whisper.transcribe")
-
-
-def print_result(audio_name, lang, text, time, expect, note):
-    print(f"\n{audio_name}")
-    print(f"Language: {lang}")
-    print(f"Transcription: {text}")
-    print(f"Expected: {expect}")
-    print(f"Note (Chinese translation): {note}")
-    print(f"Execution time: {time:.2f} seconds")
 
 
 def transcribe_audio(file_path, model="tiny", device="cpu", expect="", note=""):
@@ -26,11 +14,9 @@ def transcribe_audio(file_path, model="tiny", device="cpu", expect="", note=""):
         file_path (str): Path to the audio file to be transcribed.
         model (str): The Whisper model to use for transcription. Default is "tiny". can be tiny, small, medium, large, large-v2, large-v3
         device (str): The device to run the model on. Default is "cpu". Can be "cpu" or "cuda".
-        expect (str): The expected correct transcription of the original audio. Default is an empty string.
-        note (str): The correct Chinese translation of the audio content. Default is an empty string.
 
     Returns:
-        tuple: A tuple containing the transcribed text, the detected language, the execution time, the expected text, and the note.
+        tuple: A tuple containing the transcribed text, the detected language, and the execution time.
 
     Raises:
         FileNotFoundError: If the specified audio file does not exist.
@@ -66,31 +52,60 @@ def transcribe_audio(file_path, model="tiny", device="cpu", expect="", note=""):
             # transcribed_text = cc.convert(transcribed_text)
             pass
 
-    return transcribed_text, detected_language, execution_time, expect, note
+    return transcribed_text, detected_language, execution_time
 
 
 if __name__ == "__main__":
     folder = "/home/kent/dev/playgroud/speech-translate/test-data"
     audio_file_1 = os.path.join(folder, "sample-zh-01.mp3")
-    text_1, lang_1, time_1, expect_1, note_1 = transcribe_audio(audio_file_1, model="tiny", expect="中文語音辨識測試", note="Chinese speech recognition test")
-    print_result("Audio 1 (Chinese 1)", lang_1, text_1, time_1, expect_1, note_1)
+    text_1, lang_1, time_1 = transcribe_audio(
+        audio_file_1, model="tiny", expect="中文語音辨識測試", note="中文語音辨識測試"
+    )
+    print(f"Audio 1 - Language: {lang_1}")
+    print(f"Transcription: {text_1}")
+    print(f"Execution time: {time_1:.2f} seconds\n")
 
     audio_file_2 = os.path.join(folder, "sample-zh-02.mp3")
-    text_2, lang_2, time_2 = transcribe_audio(audio_file_2, model="tiny")
-    print_result("Audio 2 (Chinese 2)", lang_2, text_2, time_2)
+    text_2, lang_2, time_2 = transcribe_audio(
+        audio_file_2, model="tiny", expect="吃葡萄不吐葡萄皮,不吃葡萄倒吐葡萄皮", note="吃葡萄不吐葡萄皮,不吃葡萄倒吐葡萄皮"
+    )
+    print(f"Audio 2 - Language: {lang_2}")
+    print(f"Transcription: {text_2}")
+    print(f"Execution time: {time_2:.2f} seconds")
+
+    # Hello! This is a English audio speech recongnition testing.
 
     audio_file_3 = os.path.join(folder, "sample-en-01.mp3")
-    text_3, lang_3, time_3 = transcribe_audio(audio_file_3, model="tiny")
-    print_result("Audio 3 (English)", lang_3, text_3, time_3)
+    text_3, lang_3, time_3 = transcribe_audio(
+        audio_file_3, model="tiny", expect="Hello! This is a English audio speech recongnition testing", note="哈囉! 這是一個英文語音辨識測試"
+    )
+    print(f"Audio 2 - Language: {lang_3}")
+    print(f"Transcription: {text_3}")
+    print(f"Execution time: {time_3:.2f} seconds")
 
+    # 感受痛苦吧:  痛みを感じる
     audio_file_4 = os.path.join(folder, "sample-jp-01.mp3")
-    text_4, lang_4, time_4 = transcribe_audio(audio_file_4, model="tiny")
-    print_result("Audio 4 (Japanese)", lang_4, text_4, time_4)
+    text_4, lang_4, time_4 = transcribe_audio(
+        audio_file_4, model="tiny", expect="痛みを感じる", note="感受痛苦吧"
+    )
+    print(f"Audio 2 - Language: {lang_4}")
+    print(f"Transcription: {text_4}")
+    print(f"Execution time: {time_4:.2f} seconds")
 
+    # 生日快樂我的朋友：내 친구 생일 축하해.
     audio_file_5 = os.path.join(folder, "sample-kr-01.mp3")
-    text_5, lang_5, time_5 = transcribe_audio(audio_file_5, model="tiny")
-    print_result("Audio 5 (Korean)", lang_5, text_5, time_5)
+    text_5, lang_5, time_5 = transcribe_audio(
+        audio_file_5, model="tiny", expect="내 친구 생일 축하해", note="生日快樂我的朋友"
+    )
+    print(f"Audio 2 - Language: {lang_5}")
+    print(f"Transcription: {text_5}")
+    print(f"Execution time: {time_5:.2f} seconds")
 
+    # 今天天氣如何？วันนี้อากาศเป็นอย่างไร?
     audio_file_6 = os.path.join(folder, "sample-th-01.mp3")
-    text_6, lang_6, time_6 = transcribe_audio(audio_file_6, model="tiny")
-    print_result("Audio 6 (Thai)", lang_6, text_6, time_6)
+    text_6, lang_6, time_6 = transcribe_audio(
+        audio_file_6, model="tiny", expect="วันนี้อากาศเป็นอย่างไร?", note="今天天氣如何？)"
+    )
+    print(f"Audio 2 - Language: {lang_6}")
+    print(f"Transcription: {text_6}")
+    print(f"Execution time: {time_6:.2f} seconds")
